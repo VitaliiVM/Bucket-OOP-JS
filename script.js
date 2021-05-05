1e1f47d440185d50004b0a7b0acc7f3da8070034
VitaliiVM <69699528+VitaliiVM@users.noreply.github.com>
2021-05-05 03:13:55 +0300

    addEventListener

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 if (localStorage.getItem('cart')) {//проверяю наличие в LocalStorage
-    const cart = JSON.parse(localStorage.getItem('cart'));
-    let shopCart = new Cart(cart);
+    const cart = JSON.parse(localStorage.getItem('cart'));//получаю из LocalStorage cart
+    let shopCart = new Cart(cart);// cart массив товаров в корзине
     console.log(shopCart);
+    document.querySelector('.cart-out').innerHTML = '';//clear cart
+    document.querySelector('.cart-out').append(shopCart.render());//add cart
+
+
+    document.querySelector('.cart-out').addEventListener('click', (event) => {
+        let target = event.target;
+
+        if (target.classList.contains('delete')) {
+            shopCart.goodsDelete(target.dataset['articul']);
+            document.querySelector('.cart-out').innerHTML = '';
+            document.querySelector('.cart-out').append(shopCart.render());
+            localStorage.setItem('cart', JSON.stringify(shopCart.items));
+            return true;
+
+        } else if (target.classList.contains('plus')) {
+          shopCart.goodsPlus(target.dataset['articul']);
+          console.log(shopCart);
+          document.querySelector('.cart-out').innerHTML = '';
+          document.querySelector('.cart-out').append(shopCart.render());
+          localStorage.setItem('cart', JSON.stringify(shopCart.items));
+
+        } else if (target.classList.contains('minus')) {
+            shopCart.goodsMinus(target.dataset['articul']);
+            document.querySelector('.cart-out').innerHTML = '';
+            document.querySelector('.cart-out').append(shopCart.render());
+            localStorage.setItem('cart', JSON.stringify(shopCart.items));
+            return true;
+        }
+    });
+
+
 }
